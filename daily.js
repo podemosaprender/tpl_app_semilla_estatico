a95fab9ddb7d9dc62ebd751df7742c858f88fcf2
Podemos Aprender <38575920+podemosaprender@users.noreply.github.com>
2020-02-29 11:17:10 -0300

    TRY: guardr datos login

--- daily.js
+++ daily.js
@@ -1,12 +1,26 @@
+GhUser= null;
+GhPass= null;
+function onLogin() {
+	console.log("Pidio login", GhUser, GhPass);
+}
+
+function onUser(e) {
+	GhUser= e.target.value;
+}
+
+function onPass(e) {
+	GhPass= e.target.value;
+}
+
 function scr_daily(my) {
 	my.render= function (props,state) {
 		return h(Cmp.Container,{},
 			cmp({cmp: 'Form', error: true, children: [
 				{cmp: 'Form.Group', widths:'equal', children: [
-					{cmp: 'Form.Input', fluid: true, label:'Github User', placeholder:'username' },
-					{cmp: 'Form.Input', fluid: true, label:'Github Pass', placeholder:'pass' },
+					{cmp: 'Form.Input', fluid: true, label:'Github User', placeholder:'username', onChange: e => onUser(e) },
+					{cmp: 'Form.Input', type:'password',fluid: true, label:'Github Pass', placeholder:'pass', onChange: e => onPass(e) },
 				]},
-				{cmp: 'Form.Button', txt: 'Login'},
+				{cmp: 'Form.Button', txt: 'Login', onClick: onLogin},
 			]}
 		));
 	}
