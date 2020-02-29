8a600be914699ab4ffa3258eabfcf007b803fdcf
Podemos Aprender <38575920+podemosaprender@users.noreply.github.com>
2020-02-29 11:28:58 -0300

    TRY: mostrar repos

--- daily.js
+++ daily.js
@@ -1,9 +1,13 @@
 GhUser= null;
 GhPass= null;
 GhOpts= null;
+
+GhRepos= null;
 function onLogin() {
 	console.log("Pidio login", GhUser, "*****");
 	GhOpts= {user: GhUser, pass: GhPass};
+	return keys_file_github_p('',GhOpts)
+		.then(res => { GhRepos= res} );
 }
 
 function onUser(e) {
@@ -15,6 +19,10 @@ function onPass(e) {
 }
 
 function scr_daily(my) {
+	function uiOnLogin() {
+		onLogin().then(x => my.refresh());
+	}
+
 	my.render= function (props,state) {
 		return h(Cmp.Container,{},
 			cmp({cmp: 'Form', error: true, children: [
@@ -22,8 +30,10 @@ function scr_daily(my) {
 					{cmp: 'Form.Input', fluid: true, label:'Github User', placeholder:'username', onChange: e => onUser(e) },
 					{cmp: 'Form.Input', type:'password',fluid: true, label:'Github Pass', placeholder:'pass', onChange: e => onPass(e) },
 				]},
-				{cmp: 'Form.Button', txt: 'Login', onClick: onLogin},
+				{cmp: 'Form.Button', txt: 'Login', onClick: uiOnLogin},
 			]}
-		));
+		),
+			 GhRepos==null ? 'Todavia no cargue los repos' : {cmp: 'ul', children: GhRepos.map( d => ({cmp:'li', children: [ d.name ]})) }
+			);
 	}
 }
