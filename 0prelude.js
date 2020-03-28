1d165b73ffdce2509bf02a18b651fb8792bf62b5
devel@mauriciocap.com <devel@mauriciocap.com>
2020-03-28 01:27:06 -0300

    OK: semilla-apk

--- 0prelude.js
+++ 0prelude.js
@@ -4,14 +4,6 @@ GLOBAL= window; //U: para acceder a todo lo definido
 
 /************************************************************************** */
 //S: utiles
-function xfrmJsToGlobals(t,url) { //U: para que function sean globales
-	var src= '(async function loadJs_wrapper() {\n'+ 
-			('\n'+t).replace(/\n(async\s+)?function ([^ \(]+)/g,"\n$2= $1 function $2") +
-		'\nreturn new Promise(r => r("'+url+'"));\n})()\n'+
-		'//# sourceURL='+url+'\n';
-	return src;
-}
-
 function loadJs(url) { //U: cargar js desde js, OjO! seguridad y eval ...
 	console.log("loadJs",url);
 	var proto= (url.match(/^([^:]+):/)||[])[1] || (runtimeEnv=='cordova' ? 'file' : 'http');
@@ -137,7 +129,9 @@ function fSetValue(k,dst, xfrm) { //U: una funcion que recive e, y guarda e.targ
 	return function (e) { 
 		XY= e;
 		var v= typeof(e)=='object' && e.target 
-			? ('value' in e.target)
+			? (e.target.type=='file')
+			? e.target.files 
+			: ('value' in e.target)
 					? e.target.value 
 					: e.target.parentElement.children[0].checked //A: el checkbox es HORRIBLE
 			: e; //A: si es evento, value, sino el valor en si
@@ -367,6 +361,11 @@ COLOR= { } //U: para definir colores por nombre o funcion, ej. "FONDO" y poder c
 COLOR.azulOscuro= 'rgb(56,87,162)';
 COLOR.azulClaro= 'rgb(105,178,226)';
 COLOR.gris= 'rgb(194,195,201)';
+C_ROJIZO='#ad5d4e';
+C_AZUL='#40476d';
+//--826754--eb6534
+C_TOOLBAR= C_AZUL;
+
 
 LAYOUT= { //U: para poder definir directamente CSS y cambiarlo desde cfg
 	BG_COLOR: COLOR.gris, //U: el fondo la pagina para el sitio 
